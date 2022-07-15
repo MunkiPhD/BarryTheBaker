3234c53df21376cfba5ef1e575a3e91659e29b91
Dennis P <munkiphd@gmail.com>
2022-07-15 08:36:44 -0400

    refactored the code to be more readable

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace Interview_Refactor1
 {
     class Program
@@ -10,50 +11,73 @@ namespace Interview_Refactor1
             // it takes 3 apples, 2 lbs of sugar and 1 pound of flour to make 1 apple pie
             // this is intended to run on .NET Core
 
+            var applePieCalculator = new ApplePieQuantityCalculator();
+            int apples = 0, sugar = 0, flour = 0;
+            // this currently doesnt account for negative numbers
             do
             {
                 Console.WriteLine("How many apples do you have?");
-                var apples = Console.ReadLine();
+                if(!int.TryParse(Console.ReadLine(), out apples)){
+                    Console.WriteLine("Must be a number");
+                    continue;
+                } 
 
-                Console.WriteLine("How much sugar do you have?");
-                var sugar = Console.ReadLine();
+                Console.WriteLine("How much sugar do you have (in lbs)?");
+                if(!int.TryParse(Console.ReadLine(), out sugar)){
+                    Console.WriteLine("Must be a number");
+                    continue;
+                }   
 
-                Console.WriteLine("How many pounds of flour do you have?");
-                var _PoundsOfflour = Console.ReadLine();
+                Console.WriteLine("How much sugar do you have (in lbs)?");
+                if(!int.TryParse(Console.ReadLine(), out flour)){
+                    Console.WriteLine("Must be a number");
+                    continue;
+                }  
+                
+                int maxPies = applePieCalculator.MaxNumberOfPies(apples, sugar, flour);
+                Console.WriteLine($"\n\nYou can make: {maxPies} apple pies!");
 
-                Console.WriteLine("You can make:");
-                utility.Calc(apples, sugar, _PoundsOfflour);
+                var remainingIngredients = applePieCalculator.CalculateLeftOverIngredients(maxPies, apples, sugar, flour);
+                Console.WriteLine("You have the following ingredients left over:");
+                Console.WriteLine($"   Apples {remainingIngredients.Apples}");
+                Console.WriteLine($"   Sugar {remainingIngredients.Sugar}");
+                Console.WriteLine($"   Flour {remainingIngredients.Flour}");
 
                 Console.WriteLine("\n\nEnter to calculate, 'q' to quit!");
-            } while (!string.Equals(Console.ReadLine(), "Q"));
+            } while (!DoesUserWantToQuit());
+        }
 
+        public static bool DoesUserWantToQuit(){
+            Console.WriteLine("If you wish to quit, use [Q/q]");
+            var input = Console.ReadLine();
+            return string.Equals(input, "Q", StringComparison.OrdinalIgnoreCase);
         }
     }
 
-    public static class utility
+    public class ApplePieQuantityCalculator
     {
-        public static void Calc(string a, string b, string c)
-        {
-            try
-            {
-                var maxApples = (int.Parse(a) / 3);
-                var x = int.Parse(b) / 2;
-                var _flourLeft =  int.Parse(c);
-                var maxPies = Math.Min(Math.Min(maxApples, x), _flourLeft);
+        public int MaxNumberOfPies(int apples, int sugar, int flour){
+            var usedApples = apples / 3;
+            var usedSugar = sugar / 2;
+            var maxPies = Math.Min(Math.Min(usedApples, usedSugar), flour);
                
-                Console.WriteLine(maxPies + " apple pies!");
-
-                var leftOverA = int.Parse(a) - (maxPies * 3);
-                var leftOverB = int.Parse(b) - (maxPies * 2);
-                var leftOverC = int.Parse(c) - maxPies;
-
-                Console.WriteLine(leftOverA + " apple(s) left over, " + leftOverB + " lbs sugar left over, " + leftOverC + " lbs flour left over.");
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("error");
-            }
-
+            return maxPies;
         }
+
+        public RemainingIngredients CalculateLeftOverIngredients(int maxPies, int apples, int sugar, int flour){
+            var remainingIngredients = new RemainingIngredients(){
+                Apples = apples - (maxPies * 3),
+                Sugar = sugar - (maxPies * 2),
+                Flour = flour - maxPies
+            };
+            return remainingIngredients;
+        } 
+    }
+
+    public class RemainingIngredients
+    {
+            public int Apples {get;set;}
+            public int Sugar {get;set;}
+            public int Flour {get;set;}
     }
 }
