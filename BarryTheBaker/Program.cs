89b1c52f91d1ae72d96178a1d54c10453aced52e
Dennis P <munkiphd@gmail.com>
2022-07-26 22:48:38 -0400

    cleaned up the user input to prep for the cobbler pie

--- BarryTheBaker/Program.cs
+++ BarryTheBaker/Program.cs
@@ -5,66 +5,35 @@ namespace BarryTheBaker
 {
     class BakeryStart
     {
+        private static Dictionary<Ingredient, RecipeIngredient> userInputIngredients = new Dictionary<Ingredient, RecipeIngredient>();
         static void Main(string[] args)
         {
             // want to maximize the number of apple pies we can make.
             // it takes 3 apples, 2 lbs of sugar and 1 pound of flour to make 1 apple pie
             // this is intended to run on .NET Core
-
-            var applePieCalculator = new ApplePieQuantityCalculator();
-            int apples, sugar, flour, cinnamon, butter = 0;
-
-            
             // this currently doesnt account for negative numbers
             do
             {
-                Console.WriteLine("How many apples do you have?");
-                if(!int.TryParse(Console.ReadLine(), out apples)){
-                    Console.WriteLine("Must be a number");
-                    continue;
-                } 
+                // everytime we go through the loop, we want to make sure we clear out all the items
+                userInputIngredients.Clear();
 
-                Console.WriteLine("How much sugar do you have (in lbs)?");
-                if(!int.TryParse(Console.ReadLine(), out sugar)){
-                    Console.WriteLine("Must be a number");
-                    continue;
-                }   
+                AskUserForInput("How many apples do you have?", Ingredient.Apples, MeasurementType.Unit);
+                AskUserForInput("How much sugar do you have (in lbs)?", Ingredient.Sugar, MeasurementType.Pounds);
+                AskUserForInput("How much flour do you have (in lbs)?", Ingredient.Flour, MeasurementType.Pounds);
+                AskUserForInput("How much cinnamon do you have (in tsp)?", Ingredient.Cinnamon, MeasurementType.Tsp);
+                AskUserForInput("How much sticks of butter do you have?", Ingredient.Butter, MeasurementType.Tbsp);
+              
 
-                Console.WriteLine("How much flour do you have (in lbs)?");
-                if(!int.TryParse(Console.ReadLine(), out flour)){
-                    Console.WriteLine("Must be a number");
-                    continue;
-                }
-
-                Console.WriteLine("How much cinnamon do you have (in tsp)?");
-                if(!int.TryParse(Console.ReadLine(), out cinnamon)){
-                    Console.WriteLine("Must be a number");
-                    continue;
-                }
-
-                
-                Console.WriteLine("How much sticks of butter do you have?");
-                if(!int.TryParse(Console.ReadLine(), out butter)){
-                    Console.WriteLine("Must be a number");
-                    continue;
-                }
-                
-                var userIngredientInput = new Dictionary<Ingredient, RecipeIngredient>();
-                userIngredientInput.Add(Ingredient.Apples, new RecipeIngredient(Ingredient.Apples, apples, MeasurementType.Unit));
-                userIngredientInput.Add(Ingredient.Sugar,  new RecipeIngredient(Ingredient.Sugar, sugar, MeasurementType.Pounds));
-                userIngredientInput.Add(Ingredient.Flour,  new RecipeIngredient(Ingredient.Flour, flour, MeasurementType.Pounds));
-                userIngredientInput.Add(Ingredient.Butter,  new RecipeIngredient(Ingredient.Butter, butter * 8, MeasurementType.Tbsp));
-                userIngredientInput.Add(Ingredient.Cinnamon,  new RecipeIngredient(Ingredient.Cinnamon, cinnamon, MeasurementType.Tsp));
                 // now we need to figure out how many pies we can make
-                var pieCounts = applePieCalculator.MaxNumberOfPies(userIngredientInput);
-
+                var applePieCalculator = new ApplePieQuantityCalculator();
+                var pieCounts = applePieCalculator.MaxNumberOfPies(userInputIngredients);
 
                 // and now, lets print some stuff
                 Console.WriteLine($"\n\nYou can make: {pieCounts.Item1} apple pies!");
                 Console.WriteLine($"   {pieCounts.Item2} apple pies with cinnamon");
                 Console.WriteLine($"   {pieCounts.Item1 - pieCounts.Item2} apple pies WITHOUT cinnamon");
 
-                var remainingIngredients = applePieCalculator.CalculateLeftOverIngredients(pieCounts.Item1, userIngredientInput);
+                var remainingIngredients = applePieCalculator.CalculateLeftOverIngredients(pieCounts.Item1, userInputIngredients);
                 Console.WriteLine("You have the following ingredients left over:");
                 Console.WriteLine($"   Apples {remainingIngredients[Ingredient.Apples].Quantity}");
                 Console.WriteLine($"   Sugar {remainingIngredients[Ingredient.Sugar].Quantity}");
@@ -79,6 +48,20 @@ namespace BarryTheBaker
             Console.WriteLine("If you wish to quit, use [Q/q]");
             var input = Console.ReadLine();
             return string.Equals(input, "Q", StringComparison.OrdinalIgnoreCase);
+        }
+
+        private static void AskUserForInput(string question, Ingredient ingredient, MeasurementType measurement){
+            Console.WriteLine(question);
+            decimal userInput = 0;
+            if(decimal.TryParse(Console.ReadLine(), out userInput)){
+                // put the logic here to multiple sticks of butter into tbsp for now. Yes, this is a terrible place for it, i know
+                if(ingredient == Ingredient.Butter) {
+                    userInput *= 8;
+                }
+                userInputIngredients.Add(ingredient, new RecipeIngredient(ingredient, userInput, measurement));
+            } else {
+                Console.WriteLine("Must be a number!");
+            } 
         }
     }
 }
