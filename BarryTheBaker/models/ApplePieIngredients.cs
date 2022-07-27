ec27a5e95d05334145b3e7569570c68f1355bfc6
Dennis P <munkiphd@gmail.com>
2022-07-26 22:32:35 -0400

    removed unused class

--- BarryTheBaker/models/ApplePieIngredients.cs
+++
@@ -1,8 +0,0 @@
-    public class ApplePieIngredients
-    {
-            public int Apples;
-            public int Sugar;
-            public int Flour;
-            public int Cinnamon;
-            public int Butter; // in tbsp
-    }
