85f7d76a71b340801405c94ace5fad0c2cd03bd7
Dennis P <munkiphd@gmail.com>
2022-08-03 13:45:20 -0400

    refactor

--- BarryTheBaker/models/ApplePieQuantityCalculator.cs
+++ BarryTheBaker/models/ApplePieQuantityCalculator.cs
@@ -7,7 +7,7 @@ public class ApplePieQuantityCalculator
         }
 
         /// <summary>
-        /// 
+        /// Calculates the maximum number of apple pies you can make
         /// </summary>
         /// <param name="availableIngredients"></param>
         /// <returns>tuple with MaxPies, PiesWithCinnamon</returns>
@@ -18,40 +18,15 @@ public class ApplePieQuantityCalculator
             return Tuple.Create(results.MaxQuantity, (int) piesWithCinnamon);
         }
 
+        /// <summary>
+        /// Returns the left overs from makign the maximum number of apple pies possible
+        /// </summary>
+        /// <param name="maxPies"></param>
+        /// <param name="availableIngredients"></param>
+        /// <returns></returns>
         public IDictionary<Ingredient, RecipeIngredient> CalculateLeftOverIngredients(int maxPies, IDictionary<Ingredient, RecipeIngredient> availableIngredients){
-            //TODO: this is a little verbose, but we can refactor later
-            // figure out whole numbers of the ingredients we used
-            var usedIngredients = new Dictionary<Ingredient, RecipeIngredient>();
-            // usedIngredients.Add(Ingredient.Apples, new RecipeIngredient(Ingredient.Apples, recipe.Ingredients[Ingredient.Apples].Quantity * maxPies, recipe.Ingredients[Ingredient.Apples].Measurement));
-            // usedIngredients.Add(Ingredient.Sugar, new RecipeIngredient(Ingredient.Apples, recipe.Ingredients[Ingredient.Apples].Quantity * maxPies, recipe.Ingredients[Ingredient.Apples].Measurement));
-            // usedIngredients.Add(Ingredient.Flour, new RecipeIngredient(Ingredient.Apples, recipe.Ingredients[Ingredient.Apples].Quantity * maxPies, recipe.Ingredients[Ingredient.Apples].Measurement));
-            // usedIngredients.Add(Ingredient.Cinnamon, new RecipeIngredient(Ingredient.Apples, recipe.Ingredients[Ingredient.Apples].Quantity * maxPies, recipe.Ingredients[Ingredient.Apples].Measurement));
-            // usedIngredients.Add(Ingredient.Butter, new RecipeIngredient(Ingredient.Apples, recipe.Ingredients[Ingredient.Apples].Quantity * maxPies, recipe.Ingredients[Ingredient.Apples].Measurement));
-
-            foreach(var item in recipe.Ingredients){
-                RecipeIngredient ingredient = (RecipeIngredient) item.Value;
-                usedIngredients.Add(ingredient.Ingredient, new RecipeIngredient(ingredient.Ingredient, ingredient.Quantity * maxPies, ingredient.Measurement));
-            }
-
-            // calculate how many ingredients we have left based on what was used
-            // var remainingIngredients = new ApplePieIngredients(){
-            //     Apples = availableIngredients[Ingredient.Apples] - usedIngredients.Apples,
-            //     Sugar = availableIngredients[Ingredient.Sugar] - usedIngredients.Sugar,
-            //     Flour = availableIngredients[Ingredient.Flour] - usedIngredients.Flour,
-            //     Cinnamon = availableIngredients[Ingredient.Cinnamon] - usedIngredients.Cinnamon,
-            //     Butter = availableIngredients[Ingredient.Butter] - usedIngredients.Butter,
-            // };
-
-            var remainingIngredients = new Dictionary<Ingredient, RecipeIngredient>();
-            foreach(var item in usedIngredients){
-                RecipeIngredient ingredient = (RecipeIngredient) item.Value;
-                remainingIngredients.Add(ingredient.Ingredient, 
-                                        new RecipeIngredient(
-                                            ingredient.Ingredient,
-                                            availableIngredients[ingredient.Ingredient].Quantity - usedIngredients[ingredient.Ingredient].Quantity, 
-                                            ingredient.Measurement));
-            } 
-
-            return remainingIngredients;
+            var calculator = new RecipeCreationCalculator();
+            var results = calculator.MaxQuantity(new ApplePieRecipe(), availableIngredients);
+            return results.RemainingIngredients;
         } 
     }
